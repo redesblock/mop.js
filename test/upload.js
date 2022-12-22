f298b34c223b9a354fc3484978a743f4c19cf55a
sidtt <sidtt@users.noreply.github.com>
2022-12-22 21:39:43 +0800

    fix batchId

--- test/upload.js
+++ test/upload.js
@@ -1,10 +1,9 @@
 var MopJS = require("@redesblock/mop.js");
 
-const gateway = "http://127.0.0.1:1683/mop/"
+const gateway = "http://183.131.181.164:1683/mop/"
 
 async function test() {
-    batchId = "bc1e15ccb9e799b92a76df95b87f3169f37e31576c41df0527b3c7b420d89c14"
-    
+    batchId = "dd812517f2ecfe75d7b08e908a857c8703477949770fbb76f2244d0d90cb4a12" // 85    
     
     // UploadOptions
     // pin? 上传的数据是否固定在上传节点上。不适用于以网关模式运行的节点！
@@ -20,16 +19,16 @@ async function test() {
     // errorDocument? 错误文件名
     
     
-    var mop = new MopJS.Mop("http://127.0.0.1:1683")
+    var mop = new MopJS.Mop("http://183.131.181.164:1683")
     // Upload Directory : Voucher、路径、名称、可选参数
     const d1 = new Date()
-    var fileHash = await mop.uploadFilesFromDirectory(batchId, "file_example_MP4_1920_18MG", {indexDocument:"file_example_MP4_1920_18MG.m3u8"})
+    var fileHash = await mop.uploadFilesFromDirectory(batchId, "1", {indexDocument:"1.jpg", encrypt: false})
     const d2 = new Date()
     console.log("tag " + fileHash.tagUid)
     console.log("upload directory: " + gateway + fileHash.reference + "/")
     console.log("上传结束耗时秒数:" + parseInt(d2-d1)/1000)
     
-    var mopDebug = new MopJS.MopDebug("http://127.0.0.1:1685")
+    var mopDebug = new MopJS.MopDebug("http://183.131.181.164:1685")
     while(true) {
         const tag = await mopDebug.retrieveExtendedTag(fileHash.tagUid)
         if (tag.synced + tag.seen >= tag.stored) {
